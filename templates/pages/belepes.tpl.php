b2cbb7bd5207937e657b3efd47bd78451c0b8a8f
keitajs <126980311+keitajs@users.noreply.github.com>
2026-04-24 20:40:46 +0200

    belépés oldal design elkészítve

--- templates/pages/belepes.tpl.php
+++ templates/pages/belepes.tpl.php
@@ -1,23 +1,21 @@
-    <form action = "belep" method = "post">
-      <fieldset>
-        <legend>Bejlentkezés</legend>
-        <br>
-        <input type="text" name="felhasznalo" placeholder="felhasználó" required><br><br>
-        <input type="password" name="jelszo" placeholder="jelszó" required><br><br>
-        <input type="submit" name="belepes" value="Belépés">
-        <br>&nbsp;
-      </fieldset>
-    </form>
-    <h3>Regisztrálja magát, ha még nem felhasználó!</h2>
-    <form action = "regisztral" method = "post">
-      <fieldset>
-        <legend>Regisztráció</legend>
-        <br>
-        <input type="text" name="vezeteknev" placeholder="vezetéknév" required><br><br>
-        <input type="text" name="utonev" placeholder="utónév" required><br><br>
-        <input type="text" name="felhasznalo" placeholder="felhasználói név" required><br><br>
-        <input type="password" name="jelszo" placeholder="jelszó" required><br><br>
-        <input type="submit" name="regisztracio" value="Regisztráció">
-        <br>&nbsp;
-      </fieldset>
-    </form>
+<main>
+  <form action = "belep" method = "post">
+    <h2>Bejelentkezés</h2>
+    
+    <input type="text" name="felhasznalo" placeholder="Felhasználónév" required>
+    <input type="password" name="jelszo" placeholder="Jelszó" required>
+
+    <button type="submit">Bejelentkezés</button>
+  </form>
+
+  <form action = "regisztral" method = "post">
+    <h2>Regisztráció</h2>
+
+    <input type="text" name="vezeteknev" placeholder="Vezetéknév" required>
+    <input type="text" name="utonev" placeholder="Utónév" required>
+    <input type="text" name="felhasznalo" placeholder="Felhasználónév" required>
+    <input type="password" name="jelszo" placeholder="Jelszó" required>
+
+    <button type="submit">Regisztráció</button>
+  </form>
+</main>
