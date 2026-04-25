03e265786c76eee0d8b05eda1b6a736090f521a1
keitajs <dominikegersdorfer@gmail.com>
2026-04-25 12:21:56 +0200

    header bejelentkezett felhasználó kiírás formázva; index.tpl.php kód formázások

--- templates/index.tpl.php
+++ templates/index.tpl.php
@@ -1,12 +1,19 @@
-<?php session_start(); ?>
-<?php if(file_exists('./logicals/'.$keres['fajl'].'.php')) { include("./logicals/{$keres['fajl']}.php"); } ?>
+<?php
+session_start();
+
+if (file_exists('./logicals/'.$keres['fajl'].'.php')) {
+	include("./logicals/{$keres['fajl']}.php");
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
 	<title><?= $ablakcim['cim'] . ( (isset($ablakcim['mottó'])) ? ('|' . $ablakcim['mottó']) : '' ) ?></title>
 	<link rel="stylesheet" href="./styles/stilus.css" type="text/css">
-	<?php if(file_exists('./styles/'.$keres['fajl'].'.css')) { ?><link rel="stylesheet" href="./styles/<?= $keres['fajl']?>.css" type="text/css"><?php } ?>
+	<?php if (file_exists('./styles/'.$keres['fajl'].'.css')) { ?>
+		<link rel="stylesheet" href="./styles/<?= $keres['fajl']?>.css" type="text/css">
+	<?php } ?>
 </head>
 <body>
 	<header>
@@ -14,15 +21,20 @@
 			<img src="./images/<?=$fejlec['kepforras']?>" alt="<?=$fejlec['kepalt']?>">
 			<h1><?= $fejlec['cim'] ?></h1>
 		</div>
-		<?php if(isset($_SESSION['login'])) { ?>Bejlentkezve: <strong><?= $_SESSION['csn']." ".$_SESSION['un']." (".$_SESSION['login'].")" ?></strong><?php } ?>
 
 		<nav>
 			<?php foreach ($oldalak as $url => $oldal) { ?>
-				<?php if(! isset($_SESSION['login']) && $oldal['menun'][0] || isset($_SESSION['login']) && $oldal['menun'][1]) { ?>
+				<?php if (! isset($_SESSION['login']) && $oldal['menun'][0] || isset($_SESSION['login']) && $oldal['menun'][1]) { ?>
 					<a href="<?= ($url == '/') ? '.' : $url ?>" class="<?= (($oldal == $keres) ? 'active' : '') ?>">
 						<?= $oldal['szoveg'] ?>
 					</a>
 				<?php } ?>
+			<?php } ?>
+
+			<?php if (isset($_SESSION['login'])) { ?>
+				<div>
+					Bejelentkezett: <?= $_SESSION['csn'] . " " . $_SESSION['un'] . " (" . $_SESSION['login'] . ")" ?>
+				</div>
 			<?php } ?>
 		</nav>
 	</header>
@@ -32,9 +44,9 @@
 	</div>
 	
 	<footer>
-		<?php if(isset($lablec['copyright'])) { ?>&copy;&nbsp;<?= $lablec['copyright'] ?> <?php } ?>
+		<?php if (isset($lablec['copyright'])) { ?>&copy;&nbsp;<?= $lablec['copyright'] ?> <?php } ?>
 		&nbsp;
-		<?php if(isset($lablec['ceg'])) { ?><?= $lablec['ceg']; ?><?php } ?>
+		<?php if (isset($lablec['ceg'])) { ?><?= $lablec['ceg']; ?><?php } ?>
 	</footer>
 </body>
 </html>
