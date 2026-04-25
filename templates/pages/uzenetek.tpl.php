cb16729d558c6eae07497ceee9187e2b3904755d
Gergő Mátrai <35497481+redbirdjs@users.noreply.github.com>
2026-04-25 15:00:47 +0200

    üzenetek megjelenítése üzenetek oldalon

---
+++ templates/pages/uzenetek.tpl.php
@@ -0,0 +1,20 @@
+<?php
+    require "includes/db.php";
+
+    try {
+        $stmt = $dbh->prepare("SELECT * FROM uzenetek");
+        $stmt->execute();
+        $uzenetek = $stmt->fetchAll();
+    } catch (PDOException $e) {
+        die($e->getMessage());
+    }
+?>
+<main>
+    <?php foreach ($uzenetek as $uzenet): ?>
+        <div>
+            <p><?= $uzenet['uzenet'] ?></p>
+            <p><?= $uzenet['kuldo'] ?></p>
+            <p><?= $uzenet['datum'] ?></p>
+        </div>
+    <?php endforeach; ?>
+</main>
