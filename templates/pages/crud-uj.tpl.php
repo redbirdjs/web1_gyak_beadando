700c750b19bea7f10ffc85bd2e510cb15d38ec28
keitajs <dominikegersdorfer@gmail.com>
2026-04-25 12:45:17 +0200

    crud új adat hozzáadás form létrehozva

---
+++ templates/pages/crud-uj.tpl.php
@@ -0,0 +1,10 @@
+<main>
+  <form action="/logicals/uj-szoftver" method="post">
+    <h2>Új szoftver</h2>
+    
+    <input type="text" name="nev" placeholder="Név" required>
+    <input type="text" name="kategoria" placeholder="Kategória" required>
+
+    <button type="submit">Adat hozzáadása</button>
+  </form>
+</main>
