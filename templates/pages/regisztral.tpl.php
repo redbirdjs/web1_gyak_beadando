87db1a0bdaa425d7505d5e7ba18d1ef5726be4e2
keitajs <dominikegersdorfer@gmail.com>
2026-04-26 11:09:22 +0200

    regisztráció eredmény bejelentkezésre visszairányító gomb hozzáadva; tartalom középre helyezve

--- templates/pages/regisztral.tpl.php
+++ templates/pages/regisztral.tpl.php
@@ -8,6 +8,8 @@
     <a href="/"><button>Főoldal</button></a>
     <?php if($ujra) { ?>
       <a href="/belepes"><button>Vissza a regisztrációhoz</button></a>
+    <?php } else { ?>
+      <a href="/belepes"><button>Bejelentkezés</button></a>
     <?php } ?>
   </div>
 </main>
