2cfe81e7650ffc16eccef20f8d892e9e6c264a36
keitajs <dominikegersdorfer@gmail.com>
2026-04-26 14:08:02 +0200

    oldal tilte mottó hozzácsatolás logika kivéve

--- templates/index.tpl.php
+++ templates/index.tpl.php
@@ -10,7 +10,8 @@ if (file_exists('./logicals/'.$keres['fajl'].'.php')) {
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title><?= $ablakcim['cim'] . ( (isset($ablakcim['mottó'])) ? ('|' . $ablakcim['mottó']) : '' ) ?></title>
+	<title><?= $ablakcim['cim'] ?></title>
+
 	<link rel="stylesheet" href="./styles/stilus.css" type="text/css">
 	<link rel="icon" href="./images/logo.png" type="image/x-icon">
 
