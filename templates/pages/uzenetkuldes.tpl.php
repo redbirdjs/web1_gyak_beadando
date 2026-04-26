fc6932e5de3a1b9871b419a53f300accb37e64ce
Gergő Mátrai <35497481+redbirdjs@users.noreply.github.com>
2026-04-26 09:28:56 +0200

    üzenetküldés oldal kinézet

--- templates/pages/uzenetkuldes.tpl.php
+++ templates/pages/uzenetkuldes.tpl.php
@@ -1,10 +1,10 @@
 <h2>Üzenet sikeresen elküldve!</h2>
 <hr>
-<p>Üzenet adatai:</p>
-<div>
-    <p>Küldő: <?= $nev ?></p>
-    <p>Üzenet:</p>
-    <textarea><?= $_POST['uzenet'] ?></textarea>
-    <p>Küldés ideje: <?= $ido ?></p>
-    <a href="/uzenetek">Ugrás az üzenetekre</a>
+<h3>Üzenet adatai:</h3>
+<div class="elrendezes">
+    <div class="message">
+        <p><?= $_POST['uzenet'] ?></p>
+        <p class="adatok"><?= $nev ?> <span>•</span> <?= $ido ?></p>
+    </div>
+    <a href="/uzenetek"><button class="btn">Ugrás az üzenetekre</button></a>
 </div>
