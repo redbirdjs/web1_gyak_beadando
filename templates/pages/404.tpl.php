e118fec96e54bca9c76ef03562d7c212431b8762
Gergő Mátrai <35497481+redbirdjs@users.noreply.github.com>
2026-04-26 13:02:21 +0200

    404 oldal tartalom és design

--- templates/pages/404.tpl.php
+++ templates/pages/404.tpl.php
@@ -1,1 +1,10 @@
-<h2><?= $keres['szoveg'] ?></h2>
+<?php
+$url = explode('&', $_SERVER['QUERY_STRING'])[0];
+?>
+
+<div class="container">
+    <h2>404</h2>
+    <h3><?= $keres['szoveg'] ?></h3>
+    <p>Az általad keresett oldal (<?= $url ?>) nem található.</p>
+    <a href="/"><button class="btn">Vissza a főoldalra</button></a>
+</div>
